4a3eb9bc3333ae53e4c49502fcf145d07ea9df32
subzero.army <subzero.army16@gmail.com>
2026-08-16 18:34:39 +0700

    fix(ci): fix build engine for Next.js static export and GitHub Pages path routing

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,17 @@
 import type { NextConfig } from "next";
 
+const isGithubActions = process.env.GITHUB_ACTIONS || false;
+
+let repo = "";
+if (isGithubActions) {
+  const repoName = process.env.GITHUB_REPOSITORY?.split("/")[1] || "karya1.0";
+  repo = `/${repoName}`;
+}
+
 const nextConfig: NextConfig = {
   output: "export",
+  basePath: repo,
+  assetPrefix: repo ? `${repo}/` : "",
   trailingSlash: true,
   images: {
     unoptimized: true,
